7278a796985123d2450d76b03e1c288328a1d8e6
Murat Altun <mraltun@gmail.com>
2022-09-09 18:14:09 +0300

    Added Image files and Inputs in the Register page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Home from "./pages/Home";
+import "./style.scss";
 
 const App = () => {
   return (
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Add from "../img/addAvatar.png";
 
 const Login = () => {
   return <div>Login</div>;
