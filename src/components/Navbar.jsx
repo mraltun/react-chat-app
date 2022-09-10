38784259769ec711357b0d5df5212d4e4c96231e
Murat Altun <mraltun@gmail.com>
2022-09-10 13:17:43 +0300

    Navbar component done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 const Navbar = () => {
-  return <div className='navbar'>Navbar</div>;
+  return (
+    <div className='navbar'>
+      <span className='logo'>Chat</span>
+      <div className='user'>
+        <img
+          src='https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80'
+          alt=''
+        />
+        <span>Murat</span>
+        <button>logout</button>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
