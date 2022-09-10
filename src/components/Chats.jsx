2b6eeffb0252fdd7d3d681aa141e9da71a9e7f2d
Murat Altun <mraltun@gmail.com>
2022-09-10 14:02:29 +0300

    Added placeholders to Chat and Chats components

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -13,6 +13,26 @@ const Chats = () => {
           <p>Hello</p>
         </div>
       </div>
+      <div className='userChat'>
+        <img
+          src='https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80'
+          alt=''
+        />
+        <div className='userChatInfo'>
+          <span>Jane</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className='userChat'>
+        <img
+          src='https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80'
+          alt=''
+        />
+        <div className='userChatInfo'>
+          <span>Jane</span>
+          <p>Hello</p>
+        </div>
+      </div>
     </div>
   );
 };
