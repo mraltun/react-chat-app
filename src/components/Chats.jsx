a31fcc33d6e97e2ef0272358915b275e41c8b2e0
Murat Altun <mraltun@gmail.com>
2022-09-10 13:36:27 +0300

    Added placeholders to Search and Chats components

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 const Chats = () => {
-  return <div>Chats</div>;
+  return (
+    <div className='chats'>
+      <div className='userChat'>
+        <img
+          src='https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80'
+          alt=''
+        />
+        <div className='userChatInfo'>
+          <span>Jane</span>
+          <p>Hello</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Chats;
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 
 const Search = () => {
-  return <div>Search</div>;
+  return (
+    <div className='search'>
+      <div className='searchForm'>
+        <input type='text' placeholder='find a user' />
+      </div>
+      <div className='userChat'>
+        <img
+          src='https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80'
+          alt=''
+        />
+        <div className='userChatInfo'>
+          <span>Jane</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Search;
