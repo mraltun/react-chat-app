1034b302fb9570e77c96b68e74a5680e687366e9
Murat Altun <mraltun@gmail.com>
2022-09-11 19:44:07 +0300

    Search functonality

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
 import { auth } from "../firebase";
 import { signOut } from "firebase/auth";
+import { AuthContext } from "../context/AuthContext";
 
 const Navbar = () => {
+  const { currentUser } = useContext(AuthContext);
+
   return (
     <div className='navbar'>
       <span className='logo'>Chat</span>
       <div className='user'>
-        <img
-          src='https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80'
-          alt=''
-        />
-        <span>Murat</span>
+        <img src={currentUser.photoURL} alt='' />
+        <span>{currentUser.displayName}</span>
         <button onClick={() => signOut(auth)}>logout</button>
       </div>
     </div>
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Search = () => {
+  const [username, setUsername] = useState("");
+  const [user, setUser] = useState(null);
+  const [error, setError] = useState(false);
+
+  const handleSearch = (e) => {
+    // 1:20:30
+  };
+
+  const handleKey = (e) => {
+    e.code === "Enter" && handleSearch();
+  };
+
   return (
     <div className='search'>
       <div className='searchForm'>
-        <input type='text' placeholder='Find a user' />
+        <input
+          type='text'
+          placeholder='Find a user'
+          onKeyDown={handleKey}
+          onChange={(e) => setUsername(e.target.value)}
+        />
       </div>
       <div className='userChat'>
         <img
