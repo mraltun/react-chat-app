85b2d0448b1700c1749d435d6c3b148b9d590af6
Murat Altun <mraltun@gmail.com>
2022-09-11 12:11:11 +0300

    Message component is done

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 
 const Message = () => {
-  return <div>Message</div>;
+  return (
+    <div className='message'>
+      <div className='messageInfo'>
+        <img
+          src='https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80'
+          alt=''
+        />
+        <span>just now</span>
+      </div>
+      <div className='messageContent'>
+        <p>hello</p>
+        {/* <img
+          src='https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80'
+          alt=''
+        /> */}
+      </div>
+    </div>
+  );
 };
 
 export default Message;
