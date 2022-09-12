28dd64eb2d705c64852e4f028683b8b00cc409f2
Murat Altun <mraltun@gmail.com>
2022-09-12 14:49:20 +0300

    Added sort in Chats

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,8 +1,15 @@
 import React, { useContext, useState } from "react";
 import { AuthContext } from "../context/AuthContext";
 import { ChatContext } from "../context/ChatContext";
-import { arrayUnion, doc, Timestamp, updateDoc } from "firebase/firestore";
-import { db } from "../firebase";
+import {
+  arrayUnion,
+  doc,
+  serverTimestamp,
+  Timestamp,
+  updateDoc,
+} from "firebase/firestore";
+import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
+import { db, storage } from "../firebase";
 import { v4 as uuid } from "uuid";
 import Img from "../img/img.png";
 import Attach from "../img/attach.png";
@@ -16,16 +23,52 @@ const Input = () => {
 
   const handleSend = async () => {
     if (img) {
+      const storageRef = ref(storage, uuid());
+      const uploadTask = uploadBytesResumable(storageRef, img);
+
+      uploadTask.on(
+        (error) => {},
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
+            await updateDoc(doc(db, "chats", data.chatId), {
+              messages: arrayUnion({
+                id: uuid(),
+                text,
+                senderId: currentUser.uid,
+                date: Timestamp.now(),
+                img: downloadURL,
+              }),
+            });
+          });
+        }
+      );
+    } else {
       await updateDoc(doc(db, "chats", data.chatId), {
         messages: arrayUnion({
-          id: uuid,
+          id: uuid(),
           text,
           senderId: currentUser.uid,
           date: Timestamp.now(),
         }),
       });
-    } else {
     }
+
+    await updateDoc(doc(db, "userChats", currentUser.uid), {
+      [data.chatId + ".lastMessage"]: {
+        text,
+      },
+      [data.chatId + ".date"]: serverTimestamp(),
+    });
+
+    await updateDoc(doc(db, "userChats", data.user.uid), {
+      [data.chatId + ".lastMessage"]: {
+        text,
+      },
+      [data.chatId + ".date"]: serverTimestamp(),
+    });
+
+    setText("");
+    setImg(null);
   };
 
   return (
@@ -34,6 +77,7 @@ const Input = () => {
         type='text'
         placeholder='Type something..'
         onChange={(e) => setText(e.target.value)}
+        value={text}
       />
       <div className='send'>
         <img src={Attach} alt='' />
