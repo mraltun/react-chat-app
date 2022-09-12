0ca917f20e5a58b038615a0209ad9ca7018a5896
Murat Altun <mraltun@gmail.com>
2022-09-12 13:06:20 +0300

    Implemented getting the chats from Firebase

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,38 +1,39 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
+import { doc, onSnapshot } from "firebase/firestore";
+import { db } from "../firebase";
+import { AuthContext } from "../context/AuthContext";
 
 const Chats = () => {
+  const [chats, setChats] = useState([]);
+  const { currentUser } = useContext(AuthContext);
+
+  useEffect(() => {
+    const getChats = () => {
+      const unsub = onSnapshot(doc(db, "userChats", currentUser.uid), (doc) => {
+        setChats(doc.data());
+      });
+
+      return () => {
+        unsub();
+      };
+    };
+
+    currentUser.uid && getChats();
+  }, [currentUser.uid]);
+
   return (
     <div className='chats'>
-      <div className='userChat'>
-        <img
-          src='https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80'
-          alt=''
-        />
-        <div className='userChatInfo'>
-          <span>Jane</span>
-          <p>Hello</p>
-        </div>
-      </div>
-      <div className='userChat'>
-        <img
-          src='https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80'
-          alt=''
-        />
-        <div className='userChatInfo'>
-          <span>Jane</span>
-          <p>Hello</p>
-        </div>
-      </div>
-      <div className='userChat'>
-        <img
-          src='https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80'
-          alt=''
-        />
-        <div className='userChatInfo'>
-          <span>Jane</span>
-          <p>Hello</p>
-        </div>
-      </div>
+      {Object.entries(
+        chats?.map((chat) => (
+          <div className='userChat' key={chat[0]}>
+            <img src={chat[1].userInfo.photoURL} alt='' />
+            <div className='userChatInfo'>
+              <span>{chat[1].userInfo.displayName}</span>
+              <p>{chat[1].userInfo.lastMessage?.text}</p>
+            </div>
+          </div>
+        ))
+      )}
     </div>
   );
 };
